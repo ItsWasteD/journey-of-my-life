95c830e798a8fd329a45973131be4d7c8cc1e86a
David Roth <david.roth@students.bfh.ch>
2025-08-09 01:18:34 +0200

    Working on scrolltrigger animation

--- src/main/Map.tsx
+++ src/main/Map.tsx
@@ -3,6 +3,7 @@ import { gsap } from "gsap";
 import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import Panel from "./Panel";
+import "./map.css";
 
 gsap.registerPlugin(useGSAP);
 gsap.registerPlugin(ScrollTrigger);
@@ -10,13 +11,44 @@ gsap.registerPlugin(ScrollTrigger);
 export default function Main() {
 	const container = useRef(null);
 
-	useGSAP(() => {}, { scope: container });
+	useGSAP(() => {
+		gsap.set("#cross", { yPercent: "+=20" });
+
+		const tl = gsap.timeline({
+			scrollTrigger: {
+				trigger: "#cross",
+				markers: true,
+				start: "center 20%",
+				end: "1000vw top",
+				scrub: 1,
+				pin: true,
+				anticipatePin: 1,
+			},
+		});
+
+		tl.to("#cross", {
+			x: "-30vw",
+			duration: 2,
+			ease: "power1.in",
+		})
+			.to("#cross", { x: "30vw", duration: 3, ease: "back.inOut" })
+			.to("#cross", { x: 0, duration: 2, ease: "power1.out" });
+	});
 
 	return (
 		<>
-			<div ref={container}>
-				<Panel color={"green"} title={"First"} />
-				<Panel color={"red"} title={"Second"} />
+			<div id="map">
+				<img
+					id="cross"
+					src="src/assets/cross.png"
+					width={50}
+					height={50}
+					style={{
+						transform: `rotate(${Math.random() * 90}deg)`,
+					}}
+				/>
+				{/*}<Panel color={"green"} title={"First"} />
+				<Panel color={"red"} title={"Second"} />{*/}
 			</div>
 		</>
 	);
--- src/main/Panel.tsx
+++ src/main/Panel.tsx
@@ -1,6 +1,12 @@
 import "./panel.css";
 
-export default function Panel({ color, title }) {
+export default function Panel({
+	color,
+	title,
+}: {
+	color: string;
+	title: string;
+}) {
 	return (
 		<div className="content-panel" style={{ backgroundColor: color }}>
 			<h2>{title}</h2>
